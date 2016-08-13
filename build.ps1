eb452a2962b4bda9adbd1c1c4fa478d2a6e0fac2
LRFLEW <lrflew@aol.com>
2016-08-13 02:57:49 -0500

    Fix Type in Zip Name

--- build.ps1
+++ build.ps1
@@ -6,4 +6,4 @@ pushd .\libraries\boost\
   .\b2 install --prefix=..\..\ --without-python --layout=tagged address-model=32 toolset=msvc-14.0 variant=debug,release link=static threading=multi runtime-link=static
 popd
 
-7z a -tzip -mtc=off -mx=9 OpenMC2-libs-vc140.zip include lib
+7z a -tzip -mtc=off -mx=9 OpenMC2-libs-vs140.zip include lib
