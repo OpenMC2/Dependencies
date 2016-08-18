4491dda6079f1973f71ea13203c1a272b994f3fe
LRFLEW <lrflew@aol.com>
2016-08-18 02:52:49 -0500

    Made Boost a Subset Using bcp

--- build.ps1
+++ build.ps1
@@ -1,9 +1,11 @@
-git submodule update --init --recursive
+#git submodule update --init --recursive
 
+# boost is setup as a sub-set using the following command:
+# $ bcp algorithm conversion dll filesystem lexical_cast predef preprocessor program_options property_tree range timer tokenizer tuple variant scope_exit core utility build ../Dependencies/libraries/boost
 pushd .\libraries\boost\
   .\bootstrap.bat
-  .\b2 headers
-  .\b2 install --prefix=..\..\ --without-python --layout=tagged address-model=32 toolset=msvc-14.0 variant=debug,release link=static threading=multi runtime-link=static
+  #.\b2 headers
+  .\b2 install --prefix=..\..\ -j2 --layout=tagged address-model=32 toolset=msvc-14.0 variant=debug,release link=static threading=multi runtime-link=static
 popd
 
 7z a -tzip -mtc=off -mx=9 OpenMC2-libs-vs140.zip include lib
